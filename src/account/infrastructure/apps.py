c65575d51dc88e8970b8096fce2c27c4d9d3eeb5
notnotnice <fardinramezani97@gmail.com>
2024-11-14 09:42:39 -0800

    organizing the folders structure and creating the CleanAppConfig class

--- src/account/infrastructure/apps.py
+++ src/account/infrastructure/apps.py
@@ -1,6 +1,6 @@
-from django.apps import AppConfig
+from src.apps_config import CleanAppConfig
 
 
-class AccountConfig(AppConfig):
+class AccountConfig(CleanAppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'account'
